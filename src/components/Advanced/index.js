00bad4470cb6739b108c1b9740b67fde6034f831
Olamide Anifowoshe <74327981+LamideAnifowoshe@users.noreply.github.com>
2023-03-12 08:39:04 +0100

    Update Advanced index.js

--- src/components/Advanced/index.js
+++ src/components/Advanced/index.js
@@ -3,36 +3,55 @@ import styled from "styled-components";
 function Advanced() {
   return (
     <AdvancedDiv>
-      <HeaderTxt>Advanced Statistics</HeaderTxt>
-      <HeaderSmallTxt>
-        Track how your links are performing accross the web with <br />
-        our advanced statistics dashboard.
-      </HeaderSmallTxt>
-      <CardDiv>
-        <Card>
-          <HeaderTxt style={{ fontSize: "20px" }}>Brand Recognition</HeaderTxt>
-          <HeaderSmallTxt>
-            Boost your brand recognition witheach click. Generic clicks don't
-            mean a thing. Branded links help instil confidence in your content.
-          </HeaderSmallTxt>
-        </Card>
+      <AdvancedHeaderDiv>
+        <HeaderTxt>Advanced Statistics</HeaderTxt>
+        <HeaderSmallTxt>
+          Track how your links are performing accross the web with <br />
+          our advanced statistics dashboard.
+        </HeaderSmallTxt>
+      </AdvancedHeaderDiv>
 
-        <Card style={{ marginTop: "1rem" }}>
-          <HeaderTxt style={{ fontSize: "20px" }}>Brand Recognitiom</HeaderTxt>
-          <HeaderSmallTxt>
-            Boost your brand recognition witheach click. Generic clicks don't
-            mean a thing. Branded links help instil confidence in your content.
-          </HeaderSmallTxt>
-        </Card>
+      <CardDivs>
+        <CardDiv>
+          <Card style={{ marginTop: "2rem" }}>
+            <HeaderTxt style={{ fontSize: "20px" }}>
+              Brand Recognition
+            </HeaderTxt>
+            <HeaderSmallTxt>
+              Boost your brand recognition witheach click. Generic clicks don't
+              mean a thing. Branded links help instil confidence in your
+              content.
+            </HeaderSmallTxt>
+          </Card>
+          <CardImg src="/icon-brand-recognition.png" alt="img" />
+        </CardDiv>
 
-        <Card style={{ marginTop: "2rem" }}>
-          <HeaderTxt style={{ fontSize: "20px" }}>Brand Recognitiom</HeaderTxt>
-          <HeaderSmallTxt>
-            Boost your brand recognition witheach click. Generic clicks don't
-            mean a thing. Branded links help instil confidence in your content.
-          </HeaderSmallTxt>
-        </Card>
-      </CardDiv>
+        <CardDiv>
+          <Card style={{ marginTop: "3rem" }}>
+            <HeaderTxt style={{ fontSize: "20px" }}>
+              Brand Recognitiom
+            </HeaderTxt>
+            <HeaderSmallTxt>
+              Boost your brand recognition witheach click. Generic clicks don't
+              mean a thing. Branded links help instil confidence in your
+              content.
+            </HeaderSmallTxt>
+          </Card>
+        </CardDiv>
+
+        <CardDiv>
+          <Card style={{ marginTop: "4rem" }}>
+            <HeaderTxt style={{ fontSize: "20px" }}>
+              Brand Recognitiom
+            </HeaderTxt>
+            <HeaderSmallTxt>
+              Boost your brand recognition witheach click. Generic clicks don't
+              mean a thing. Branded links help instil confidence in your
+              content.
+            </HeaderSmallTxt>
+          </Card>
+        </CardDiv>
+      </CardDivs>
     </AdvancedDiv>
   );
 }
@@ -41,40 +60,48 @@ const AdvancedDiv = styled.div`
   margin: 3rem 0;
 `;
 
+const AdvancedHeaderDiv = styled.div`
+  text-align: center;
+`;
+
 const HeaderTxt = styled.h1`
   font-size: 30px;
   color: hsl(260, 8%, 14%);
-  text-align: center;
 `;
 
 const HeaderSmallTxt = styled.h4`
   color: hsl(0, 0%, 75%);
-  font-size: 15px;
-  text-align: center;
+  font-size: 18px;
 `;
 
-const CardDiv = styled.div`
+const CardDivs = styled.div`
   display: flex;
   justify-content: space-around;
 `;
 
-const Card = styled.div`
-  width: 20%;
+const CardDiv = styled.div`
+  width: 30%;
   height: 200px;
   position: relative;
-  padding: 1rem 2rem;
+  padding: 2rem;
+`;
+
+const Card = styled.div`
+  padding: 2rem;
   background-color: white;
-  box-shadow: 1px 1px 1px 1px rgba(0, 0, 0, 0.1),
-    1px 1px 1px 1px rgba(0, 0, 0, 0.1);
-  text-align: center;
+  box-shadow: 5px 1px 1px 5px rgba(0, 0, 0, 0.1),
+    5px 1px 1px 5px rgba(0, 0, 0, 0.1);
   border-radius: 0.5rem;
 `;
 
 const CardImg = styled.img`
   background-color: hsl(260, 8%, 14%);
+  padding: 1.5rem;
   border: none;
-  border-radius: 5rem;
+  border-radius: 50px;
   position: absolute;
+  top: 2px;
+  left: 7rem;
 `;
 
 export default Advanced;
